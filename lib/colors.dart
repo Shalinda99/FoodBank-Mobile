d478c0190e4572a82796f3c88306a850556e5d59
Milanka00 <milankatharangana01@gmail.com>
2023-06-07 21:34:37 +0530

    color changed

--- lib/colors.dart
+++ lib/colors.dart
@@ -9,18 +9,18 @@ class ColorPalette {
   static const Color darkOrange = Color(0xFFFF8C00);
 
   static const Color green = Color(0xFF2FC4B2);
-  static const Color darkGreen = Color(0xFF117C6F);
-  static const Color lightGreen = Color(0xFF8DE5DB);
+  static const Color darkGreen = Color.fromARGB(255, 13, 106, 95);
+  static const Color lightGreen = Color(0xFF8DEFD8);
 
   //static const Color yellow = Color(0xFFFFFF00);
   static const Color yellow = Color(0xFFFFD801);
   static const Color lightYellow = Color(0xFFFFFFE0);
- // static const Color darkYellow = Color(0xFF808000);
- static const Color darkYellow = Color(0xFFFFA722);
+  // static const Color darkYellow = Color(0xFF808000);
+  static const Color darkYellow = Color(0xFFFFA722);
 
- // static const Color blue = Color(0xFF0000FF);
- static const Color blue = Color(0xFF00bfe0);
-  static const Color lightBlue = Color(0xFFADD8E6);
+  // static const Color blue = Color(0xFF0000FF);
+  static const Color blue = Color.fromARGB(255, 63, 141, 155);
+  static const Color lightBlue = Color.fromARGB(255, 26, 68, 82);
   // static const Color darkBlue = Color(0xFF00008B);
-   static const Color darkBlue = Color(0xFF006D8F);
+  static const Color darkBlue = Color.fromARGB(255, 4, 13, 59);
 }
