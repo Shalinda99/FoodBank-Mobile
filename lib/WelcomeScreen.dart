71e03affad9aaf52f01c5eb9bc27e61ac14fc21b
Milanka00 <milankatharangana01@gmail.com>
2023-06-07 20:12:57 +0530

    check errors -1 m

---
+++ lib/WelcomeScreen.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/HomeScreen.dart';
+import 'colors.dart' as colors;
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 1.6,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height / 1.6,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          colors.ColorPalette.green,
+                          colors.ColorPalette.lightGreen,
+                        ],
+                      ),
+                      // color: Colors.orange,
+                      borderRadius:
+                          BorderRadius.only(bottomRight: Radius.circular(70)),
+                    ),
+                    child: Center(
+                      child: Image.asset(
+                        "assets/img4.png",
+                        scale: 0.8,
+                      ),
+                    )),
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 2.666,
+                decoration: BoxDecoration(
+                  color: colors.ColorPalette.green,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 2.666,
+                padding: EdgeInsets.only(top: 40, bottom: 30),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(70)),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      "Donate Food & Money online",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1,
+                        wordSpacing: 2,
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: Text(
+                        "Together, we can make a better tomorrow a reality",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.black.withOpacity(0.6),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 40),
+                    Material(
+                      color: colors.ColorPalette.darkGreen,
+                      borderRadius: BorderRadius.circular(10),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => HomeScreen(),
+                              ));
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 70),
+                          child: Text(
+                            "Get Start",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Color.fromARGB(255, 255, 255, 255)
+                                  .withOpacity(0.9),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
