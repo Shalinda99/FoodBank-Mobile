4657fc6d30bb8993ade5b0100ceafbbe06b6aa62
Milanka00 <milankatharangana01@gmail.com>
2023-06-07 20:18:38 +0530

    v

---
+++ lib/navBar.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Projects.dart';
+import 'package:flutter_application_1/fooddonation.dart';
+import 'package:flutter_application_1/moneyDonation.dart';
+import 'colors.dart' as colors;
+
+class NavBar extends StatelessWidget {
+  const NavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        child: ListView(
+      padding: EdgeInsets.zero,
+      children: [
+        const SizedBox(
+            height: 110,
+            child: UserAccountsDrawerHeader(
+              accountName: Text(
+                'FOOD BANK',
+                style: TextStyle(
+                    fontSize: 25.0, color: Color.fromARGB(255, 11, 13, 46)),
+              ),
+
+              // ignore: prefer_const_constructors
+              decoration: BoxDecoration(
+                color: colors.ColorPalette.green,
+              ),
+              accountEmail: Text(
+                'Feed The Need',
+                style: TextStyle(
+                    fontSize: 15.0, color: Color.fromARGB(255, 11, 13, 46)),
+              ),
+            )),
+        ListTile(
+          leading: const Icon(Icons.home_filled),
+          title: const Text('About Us'),
+          // ignore: avoid_returning_null_for_void
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Projects(),
+                ));
+            // Add your code for when this image is tapped.
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.question_mark_outlined),
+          title: const Text('Why Food Bank?'),
+          // ignore: avoid_returning_null_for_void
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Projects(),
+                ));
+            // Add your code for when this image is tapped.
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.notes_outlined),
+          title: const Text('Projects'),
+          // ignore: avoid_returning_null_for_void
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Projects(),
+                ));
+            // Add your code for when this image is tapped.
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.food_bank_sharp),
+          title: const Text('Donate Food'),
+          // ignore: avoid_returning_null_for_void
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => FoodDonation(),
+                ));
+            // Add your code for when this image is tapped.
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.money),
+          title: const Text('Donate Money'),
+          // ignore: avoid_returning_null_for_void
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => moneyDonation(),
+                ));
+            // Add your code for when this image is tapped.
+          },
+        )
+      ],
+    ));
+  }
+}
+
+class Boxfit {}
